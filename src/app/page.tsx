56561bbdbf575478d55a54585612ac4806adf46a
Arun Karitala <arun@Aruns-MacBook-Air.local>
2026-03-29 14:54:44 -0400

    chore: hide testimonials section on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <Industries />
         <Process />
         <CaseStudies />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <CTABanner />
         <Contact />
       </main>
